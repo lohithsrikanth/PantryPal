e28712345601cd2f2f00b73d7fdb4961a880468d
Lohith Srikanth <srikanthlohith@gmail.com>
2023-04-23 07:41:13 -0400

    Modify the RecipeService class implementation

--- Microservices/Microservice1/RecipeService.cpp
+++ Microservices/Microservice1/RecipeService.cpp
@@ -8,44 +8,12 @@
 using json = nlohmann::json;
 using namespace std;
 
-size_t RecipeService::write_callback(char* ptr, size_t size, size_t nmemb, string* userdata) {
-    userdata->append(ptr, size * nmemb);
-    return size * nmemb;
-}
-
 json RecipeService::readFile(string fileName) {
     ifstream file(fileName);
 
     json jsonData;
     file >> jsonData;
     return jsonData;
-}
-
-json RecipeService::makeRequest(string& url) {
-    string response;
-    CURL *curl_ = curl_easy_init();
-
-    curl_easy_setopt(curl_, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
-    curl_slist* headers = nullptr;
-    
-    headers = curl_slist_append(headers, "X-RapidAPI-Key: 7ba169ddc1msh262f271b67b1d7cp1c6d27jsn5557275e88f3");
-    headers = curl_slist_append(headers, "X-RapidAPI-Host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
-    curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &response);
-    
-    CURLcode res = curl_easy_perform(curl_);
-    
-    if (res != CURLE_OK) {
-        throw runtime_error(curl_easy_strerror(res));
-    }
-
-    curl_easy_cleanup(curl_);
-    curl_slist_free_all(headers);
-
-    json responseJson = json::parse(response);
-    return responseJson;
 }
 
 vector<Recipe> RecipeService::findRecipesByIngredients(const vector<string>& ingredients, int number, bool ignorePantry, int ranking) {
@@ -63,7 +31,6 @@ vector<Recipe> RecipeService::findRecipesByIngredients(const vector<string>& ing
     int count = 0;
     for (const auto& recipeJson : responseJson) {
         // only include recipes that have the missedIngredientCount as 0
-
         if (recipeJson["missedIngredientCount"] == 0) {
             Recipe recipe;
             recipe.setId(recipeJson["id"]);
@@ -72,14 +39,8 @@ vector<Recipe> RecipeService::findRecipesByIngredients(const vector<string>& ing
             recipes.push_back(recipe);
             ++count;
         }
-        // Recipe recipe;
-        // recipe.setId(recipeJson["id"]);
-        // recipe.setName(recipeJson["title"]);
-        // getRecipeInformation(recipe);
-        // recipes.push_back(recipe);
-        // count++;
-        
     }
+
     return recipes;
 }
 
