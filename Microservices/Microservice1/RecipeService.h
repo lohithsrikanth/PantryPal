f01798b826ae7b63dd3d584e9d7548e536768d54
Lohith Srikanth <srikanthlohith@gmail.com>
2023-04-22 20:55:52 -0400

    Add header file for the RecipeService class

--- Microservices/Microservice1/RecipeService.h
+++ Microservices/Microservice1/RecipeService.h
@@ -12,17 +12,15 @@ using json = nlohmann::json;
 
 class RecipeService {
 public:
-    RecipeService();
-    ~RecipeService();
-
-    vector<Recipe> findRecipesByIngredients(const vector<string> ingredients, int number=5, 
-                                        bool ignorePantry=false, int ranking=1);
+    vector<Recipe> findRecipesByIngredients(const vector<string>& ingredients, int number=100, 
+                                        bool ignorePantry=true, int ranking=1);
 
 private:
     CURL* curl_;
     static size_t write_callback(char* ptr, size_t size, size_t nmemb, string* userdata);
     json makeRequest(string& url);
     void getRecipeInformation(Recipe& recipe);
+    json readFile(string fileName);
 };   
 
 #endif
